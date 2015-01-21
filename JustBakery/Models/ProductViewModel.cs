beaba0de23b71ecdcadbf8e28ee8b27c39224df9
Александр Распопов <rsppv.av@gmail.com>
2015-01-21 14:28:34 +0600

    Added view product by categories. Forgot ViewModel

---
+++ JustBakery/Models/ProductViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace JustBakery.Models
+{
+  public class ProductViewModel
+  {
+    public IEnumerable<ProductType> Categories { get; set; }
+    public IEnumerable<Product> Products { get; set; }
+  }
+}
