0c38b008092a3eb6ec93d395864802d42a2a1c24
rsppv <rsppv.av@gmail.com>
2015-01-28 17:07:43 +0400

    metadata files for generated classes

---
+++ JustBakery/Models/DetailRawOperationModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JustBakery.Models
+{
+  [MetadataType(typeof(DetailRawOperationMD))]
+  public partial class DetailRawOperation
+  {
+  }
+
+  public class DetailRawOperationMD
+  {
+    [Key]
+    public System.Guid RecordID { get; set; }
+    public System.Guid RawLogRecordID { get; set; }
+    public System.Guid RawID { get; set; }
+    public int Count { get; set; }
+
+    public virtual Raw Raw { get; set; }
+    public virtual RawAccountingLog RawAccountingLog { get; set; }    
+  }
+}
---
+++ JustBakery/Models/RowAccountingLogModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JustBakery.Models
+{
+  [MetadataType(typeof(RawAccountingLogMD))]
+  public partial class RawAccountingLog
+  {
+  }
+
+  public class RawAccountingLogMD
+  {
+    [Key]
+    public System.Guid LogRecordID { get; set; }
+    public System.Guid OperationTypeID { get; set; }
+    public System.Guid StockID { get; set; }
+    public Nullable<System.Guid> SupplierID { get; set; }
+    public Nullable<System.Guid> EmployeeID { get; set; }
+    public System.DateTime OperationDate { get; set; }
+    public bool IsDeleted { get; set; }
+
+    public virtual Stock Stock { get; set; }
+    public virtual Supplier Supplier { get; set; }
+    public virtual Employee Employee { get; set; }
+    public virtual OperationType OperationType { get; set; }
+    public virtual ICollection<DetailRawOperation> DetailRawOperation { get; set; }
+  }
+}
